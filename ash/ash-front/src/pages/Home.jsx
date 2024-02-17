614e61fc16b33ea2ffb7ecf451a961b9aa943765
Chinnapak <simsimsim21@gmail.com>
2024-02-17 19:39:56 +0700

    complete test

--- ash/ash-front/src/pages/Home.jsx
+++ ash/ash-front/src/pages/Home.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
 import { MyButton } from "../components/Button";
 import "../css/Homepage.css";
@@ -13,7 +20,7 @@ export default function Home() {
       <div className="homePage">
         <Navbar className="navBarHome" style={{ padding: "0px" }}>
           <NavbarBrand>
-            <Image width={100} alt="SIT Logo Image" src="../../public/KMUTTSIT.png" />
+            <Image width={100} alt="SIT Logo Image" src="/KMUTTSIT.png" />
           </NavbarBrand>
 
           <NavbarContent className="sm:flex gap-6 " justify="center">
@@ -24,13 +31,13 @@ export default function Home() {
             </NavbarItem>
 
             <NavbarItem>
-              <Link href="#" color="foreground">
+              <Link href="/project" color="foreground">
                 Project
               </Link>
             </NavbarItem>
 
             <NavbarItem>
-              <Link href="#" color="foreground">
+              <Link href="/hardening" color="foreground">
                 Detail
               </Link>
             </NavbarItem>
@@ -44,7 +51,17 @@ export default function Home() {
 
           <NavbarContent justify="end">
             <NavbarItem>
-              <Button as={Link} href="#" variant="flat" className="text-foreground bg-transparent border-1 border-foreground rounded-md shadow-2xl">
+              <Button
+                onClick={() => {
+                  window.open(
+                    "https://www.cisecurity.org/benchmark/microsoft_windows_server"
+                  );
+                }}
+                as={Link}
+                href="#"
+                variant="flat"
+                className="text-foreground bg-transparent border-1 border-foreground rounded-md shadow-2xl"
+              >
                 Learn More
               </Button>
             </NavbarItem>
@@ -52,20 +69,28 @@ export default function Home() {
         </Navbar>
 
         <div className="homeContent">
-          <h1 className="homePagetext">Automate Server <br /> Hardening</h1>
+          <h1 className="homePagetext">
+            Automate Server <br /> Hardening
+          </h1>
           <div style={{ textAlign: "left" }}>
-            <MyButton onClick={() => { navigate("/project"); }} className="homePageBtn">
+            <MyButton
+              onClick={() => {
+                navigate("/project");
+              }}
+              className="homePageBtn"
+            >
               Get Started
             </MyButton>
           </div>
           <div>
-            <Image className="imgServer" alt="Server Image" src="../../public/iconServer.png" />
+            <Image
+              className="imgServer"
+              alt="Server Image"
+              src="/iconServer.png"
+            />
             <div class="dot"></div>
           </div>
         </div>
-
-
-
       </div>
     </>
   );
